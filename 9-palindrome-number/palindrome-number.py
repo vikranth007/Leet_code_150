18d785faa7d31bb024566761c52e2535fe399c94
Vikram <140962452+vikranth007@users.noreply.github.com>
2025-09-19 10:05:29 +0530

    Time: 25 ms (5.54%) | Memory: 18 MB (19.96%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,16 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        revnum = 0 # --> Store my reversed number 123 ---> 321
-        dup = x # ----> current x
+        rev = 0
+        dup = x
 
         while x > 0:
-            id = x % 10   # ---> 121  % 10 = 1,---> 12 % 10 = 2, ---> 1 % 10 = 1 
-            revnum = (revnum * 10) + id  # cur 0 * 10 + 1 = 1, --> 1 * 10 + 2 = 12, -->12 * 10 + 1 = 121 ---> it is my revnum
-            x = x // 10  #---> 121 // 10 = 12, 12 // 10 = 1, 1 // 10 = 0
-        
-        if dup == revnum:   # ---> dum = 1 2 1 == 1 2 1 return True / != return False
+            ld = x % 10
+            rev = (rev * 10) + ld
+            x = x // 10
+        if dup == rev:
             return True
         else:
             return False
-
         
